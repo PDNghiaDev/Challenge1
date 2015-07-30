e6c9ed20037d09b0cb26ea20b4a7c5706be8cb49
PDNghiaDev <pdnghiadev@gmail.com>
2015-07-30 17:29:55 +0700

    Upload Challenge1 Android

---
+++ app/src/main/java/com/gmail/pdnghiadev/challenge1/MainActivity.java
@@ -0,0 +1,75 @@
+package com.gmail.pdnghiadev.challenge1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ImageView imgPlay = (ImageView) findViewById(R.id.imgPlay);
+        final ViewTreeObserver.OnScrollChangedListener onScrollChangedListener = new ViewTreeObserver.OnScrollChangedListener(){
+
+            @Override
+            public void onScrollChanged() {
+                imgPlay.setVisibility(View.VISIBLE);
+            }
+        };
+
+        final ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView);
+        scrollView.setOnTouchListener(new View.OnTouchListener() {
+            private ViewTreeObserver observer;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (observer == null) {
+                    observer = scrollView.getViewTreeObserver();
+                    observer.addOnScrollChangedListener(onScrollChangedListener);
+                }
+                else if (!observer.isAlive()) {
+                    observer.removeOnScrollChangedListener(onScrollChangedListener);
+                    observer = scrollView.getViewTreeObserver();
+                    observer.addOnScrollChangedListener(onScrollChangedListener);
+                }
+
+                return false;
+            }
+        });
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
